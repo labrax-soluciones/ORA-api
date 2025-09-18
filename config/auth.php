3c102c0e4dc875a67b98867b31a8d32b97c0c69e
andresvieitez <andres@vieitez.es>
2025-09-18 10:50:54 +0200

    jwt

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'jwt',   // <- importante
+            'provider' => 'users',
+        ],
     ],
 
     /*
